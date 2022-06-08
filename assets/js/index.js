5e43dbd830c53ad84849317624f2b366e38dcbea
Miraska <102532370+Miraska@users.noreply.github.com>
2022-06-09 01:00:53 +0300

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,6 +11,12 @@ $(function() {
   let nav__icons = $("#nav__icons");
   let call__button = $("#call__button");
   let navToggle = $("#navToggle");
+  let burger__item = $("#burger__item");
+  let nav__link1 = $("#nav__link1");
+  let nav__link2 = $("#nav__link2");
+  let nav__link3 = $("#nav__link3");
+  let nav__link4 = $("#nav__link4");
+  let body = $("#body");
 
 
   $(window).on("scroll load resize", function() {
@@ -51,11 +57,41 @@ $(function() {
   // Nav Toggle
   navToggle.on("click", function(event) {
     event.preventDefault();
-
     nav.toggleClass("show");
     nav__icons.toggleClass("show");
     call__button.toggleClass("show");
+    burger__item.toggleClass('active');
+    navToggle.toggleClass('active');
+    body.toggleClass("lock");
   });
+
+  // Nav Link
+  nav__link1.on("click", function(event) {
+    burger__item.removeClass('active');
+    navToggle.removeClass('active');
+    body.removeClass("lock");
+  });
+
+  nav__link2.on("click", function(event) {
+    burger__item.removeClass('active');
+    navToggle.removeClass('active');
+    body.removeClass("lock");
+  });
+
+  nav__link3.on("click", function(event) {
+    burger__item.removeClass('active');
+    navToggle.removeClass('active');
+    body.removeClass("lock");
+  });
+
+  nav__link4.on("click", function(event) {
+    burger__item.removeClass('active');
+    navToggle.removeClass('active');
+    body.removeClass("lock");
+  });
+
+
+ 
 
 
 }); 
--- telegram.php
+++ telegram.php
@@ -1,19 +1,18 @@
 <?php
-// telegram
-/* https://api.telegram.org/botXXXXXXXXXXXXXXXXXXXXXXX/getUpdates,
-где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
-
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $email = $_POST['email'];
 $country = $_POST['country'];
+$comment = $_POST['comment'];
 $token = "5131798485:AAFCkoPFDRtbmkNF6PO1EaXCKbS-bAVpgcQ";
 $chat_id = "-670556109";
+
 $arr = array(
-  'Имя пользователя: ' => $name,
+  'Имя покупателя: ' => $name,
   'Телефон: ' => $phone,
-  'Email' => $email,
-  'Пользователь хочет приобрести: ' => $country
+  'Email: ' => $email,
+  'Пользователь хочет приобрести: ' => $country,
+  'Комментарий покупателля: ' => $comment
 );
 
 foreach($arr as $key => $value) {
@@ -26,43 +25,5 @@ if ($sendToTelegram) {
   header('Location: index.html');
 } else {
   echo "Error";
-}
-
-// Mail
-require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer;
-$mail->CharSet = 'utf-8';
-
-$name = $_POST['name'];
-$phone = $_POST['tel'];
-$email = $_POST['email'];
-$country = $_POST['country'];
-
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'wisaptest@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'u87CJcG8MYra1enpASCX'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
-
-$mail->setFrom('wisaptest@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('miraska.007@gmail.com');     // Кому будет уходить письмо 
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
-
-$mail->Subject = 'Заявка на приобритение визы с сайта VISA PARADISE';
-$mail->Body    = ' Заказчик ' .$name . ' оставил заявку на услугу "' .$country. '" <br>Телефон заказчика: ' .$phone. '<br>Почта заказчика: ' .$email;
-$mail->AltBody = '';
-
-if(!$mail->send()) {
-    echo 'Error';
 }
 ?>
